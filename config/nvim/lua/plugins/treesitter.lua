c5b5e3d68b2b9a04a2efaf5c51790e9bac64af3b
Felipe Cardozo <fraifelipe@gmail.com>
2022-09-19 12:20:41 -0300

    fix react comments

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -15,10 +15,31 @@ require("nvim-treesitter.configs").setup({
 	},
 	highlight = {
 		enable = true,
-		use_languagetree = true
+		use_languagetree = true,
 	},
 	context_commentstring = {
 		enable = true,
+		config = {
+			javascript = {
+				__default = "// %s",
+				jsx_element = "{/* %s */}",
+				jsx_fragment = "{/* %s */}",
+				jsx_attribute = "// %s",
+				comment = "// %s",
+				__parent = {
+					-- if a node has this as the parent, use the `//` commentstring
+					jsx_expression = "// %s",
+				},
+			},
+			tsx = {
+				jsx_element = {
+					__default = "{/* %s */}",
+					__parent = {
+						parenthesized_expression = "// %s",
+					},
+				},
+			},
+		},
 	},
 	rainbow = {
 		enable = false,
@@ -26,7 +47,7 @@ require("nvim-treesitter.configs").setup({
 		max_file_lines = 1000,
 	},
 })
-require('spellsitter').setup {
-  enable = true,
-  debug = false
-}
+require("spellsitter").setup({
+	enable = true,
+	debug = false,
+})
