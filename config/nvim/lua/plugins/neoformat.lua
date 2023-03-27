86d780f21b9a037621f9bea4b6dbd8c360aaa938
Felipe Cardozo <fraifelipe@gmail.com>
2023-03-27 16:45:30 -0300

    dotnet stuff

--- config/nvim/lua/plugins/neoformat.lua
+++ config/nvim/lua/plugins/neoformat.lua
@@ -1,2 +1,5 @@
 -- vim.g.neoformat_enabled_yaml = {"yamlfmt"}
 -- let g:neoformat_enabled_tsx = ['autopep8']
+-- vim.g.neoformat_enabled_csharp = ['csharpier']
+
+vim.g.neoformat_cs_csharpier = { exe = "csharpier", args = {}, stdin = 1 }
