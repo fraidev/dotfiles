db9d712d6bd61d42a5972d84c4bf6dba56ef11c3
Felipe Cardozo <fraifelipe@gmail.com>
2023-08-25 13:30:06 -0300

    add wezterm

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -233,6 +233,7 @@ g.lazygit_floating_window_winblend = 0 --" transparency of floating window
 g.lazygit_floating_window_scaling_factor = 0.9 --" scaling factor for floating window
 g.lazygit_floating_window_use_plenary = 1 -- " use plenary.nvim to manage floating window if available
 g.lazygit_use_neovim_remote = 1 --" fallback to 0 if neovim-remote is not installed
+
 nmap("<leader>gl", ":LazyGit<cr>")
 nmap("<leader>gf", ":LazyGitFilter<cr>")
 nmap("<leader>gc", ":LazyGitFilterCurrentFile")
