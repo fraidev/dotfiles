db5a71f7d4dd0a72f2cc15c807e4b2b794572c0d
Felipe Cardozo <fraifelipe@gmail.com>
2023-11-13 21:11:38 -0300

    remove vim is undefined warning

---
+++ config/nvim/lua/plugins/clipboard_osc52_ssh.lua
@@ -0,0 +1,15 @@
+if vim.env.SSH_TTY or vim.env.SSH_CLIENT or vim.env.SSH_CONNECTION then
+    local function copy(lines, _)
+        require("osc52").copy(table.concat(lines, "\n"))
+    end
+
+    local function paste()
+        return {vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("")}
+    end
+
+    vim.g.clipboard = {
+        name = "osc52",
+        copy = {["+"] = copy, ["*"] = copy},
+        paste = {["+"] = paste, ["*"] = paste}
+    }
+end
