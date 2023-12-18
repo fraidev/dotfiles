1381c73c75e00bf8a6b3abe2374850a51cf30020
Felipe Cardozo <fraifelipe@gmail.com>
2023-12-17 22:58:31 -0300

    remove these maps

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -23,49 +23,10 @@ cmp.setup(
             end
         },
         mapping = {
-            ["<C-n>"] = cmp.mapping(
-                {
-                    c = function()
-                        if cmp.visible() then
-                            cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
-                        else
-                            vim.api.nvim_feedkeys(t("<Down>"), "n", true)
-                        end
-                    end,
-                    i = function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
-                        else
-                            fallback()
-                        end
-                    end
-                }
-            ),
-            ["<C-p>"] = cmp.mapping(
-                {
-                    c = function()
-                        if cmp.visible() then
-                            cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
-                        else
-                            vim.api.nvim_feedkeys(t("<Up>"), "n", true)
-                        end
-                    end,
-                    i = function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
-                        else
-                            fallback()
-                        end
-                    end
-                }
-            ),
             ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {"i"}),
             ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {"i"}),
-            -- ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-            -- ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<C-j>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
             ["<C-k>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
-            -- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
             ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<CR>"] = cmp.mapping.confirm(
@@ -80,7 +41,7 @@ cmp.setup(
             {name = "nvim_lsp"},
             {name = "nvim_lsp_signature_help"},
             {name = "luasnip"},
-            -- { name = "buffer" },
+            -- {name = "buffer"},
             {name = "nvim_lua"},
             {name = "path"}
         },
