e2a68148c0f178756a32f53a98d6131466982157
Felipe Cardozo <fraifelipe@gmail.com>
2024-01-06 03:45:12 -0300

    add noice

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -210,6 +210,23 @@ require("lazy").setup(
             config = function()
                 require("plugins.trouble")
             end
+        },
+        -- Noice
+        {
+            "folke/noice.nvim",
+            event = "VeryLazy",
+            opts = {},
+            dependencies = {
+                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                "MunifTanjim/nui.nvim",
+                -- OPTIONAL:
+                --   `nvim-notify` is only needed, if you want to use the notification view.
+                --   If not available, we use `mini` as the fallback
+                "rcarriga/nvim-notify"
+            },
+            config = function()
+                require("plugins.noice")
+            end
         }
     }
 )
