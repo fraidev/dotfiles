cd2212d65151ba45321d5addf172ad77b90a1c14
Felipe Cardozo <fraifelipe@gmail.com>
2024-06-23 15:53:08 -0300

    remove obsidian

--- config/nvim/lua/custom/plugins/treesitter.lua
+++ config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,43 +1,51 @@
 -- Treesitter enables an AST-like understanding of files
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    opt = false,
-    config = function()
-        require "nvim-treesitter.configs".setup {
-            ensure_installed = {
-                -- "bash",
-                "css",
-                "html",
-                "ocaml",
-                "javascript",
-                "lua",
-                "python",
-                "rust",
-                "go",
-                "typescript",
-                "vim",
-                "tsx",
-                "json",
-                "yaml",
-                "sql",
-                "toml"
-            },
-            sync_install = false,
-            indent = {enable = true},
-            highlight = {
-                enable = true,
-                use_languagetree = true
-            },
-            -- context_commentstring = {
-            --     enable = true
-            -- },
-            rainbow = {
-                enable = false,
-                extended_mode = false,
-                max_file_lines = 1000
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false,
+        opt = false,
+        dependencies = {
+            "HiPhish/rainbow-delimiters.nvim",
+        },
+        config = function()
+            require "nvim-treesitter.configs".setup {
+                ensure_installed = {
+                    -- "bash",
+                    "css",
+                    "html",
+                    "ocaml",
+                    "javascript",
+                    "lua",
+                    "python",
+                    "rust",
+                    "go",
+                    "typescript",
+                    "vim",
+                    "tsx",
+                    "json",
+                    "yaml",
+                    "sql",
+                    "toml"
+                },
+                sync_install = false,
+                indent = {enable = true},
+                highlight = {
+                    enable = true,
+                    use_languagetree = true
+                },
+                -- context_commentstring = {
+                --     enable = true
+                -- },
+                rainbow = {
+                    enable = true,
+                    disable = {"jsx", "cpp"},
+                    -- Which query to use for finding delimiters
+                    query = "rainbow-parens",
+                    -- Highlight the entire buffer all at once
+                    max_file_lines = 3000
+                }
             }
-        }
-    end
+        end
+    }
 }
