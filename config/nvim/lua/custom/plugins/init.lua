5f3482c2666acbeb6597e84a048bbd2b7a218022
Felipe Cardozo <fraifelipe@gmail.com>
2024-09-03 17:40:49 -0300

    snippets

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -13,7 +13,19 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "kkharji/lspsaga.nvim"
+        "kkharji/lspsaga.nvim",
+        {
+            "L3MON4D3/LuaSnip",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp",
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load({paths = "./my_snippets"})
+            end
+        },
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets"
     },
     "JoosepAlviste/nvim-ts-context-commentstring",
     -- Use devicons for filetypes
@@ -109,7 +121,7 @@ return {
         "folke/todo-comments.nvim",
         dependencies = {"nvim-lua/plenary.nvim"},
         opts = {}
-    },
+    }
     -- {
     --     "nvim-neotest/neotest",
     --     dependencies = {
