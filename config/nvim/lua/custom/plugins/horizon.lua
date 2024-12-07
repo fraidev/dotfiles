65e683230263c2f6c6146d8b9bd8ddcd33952381
Felipe Cardozo <fraifelipe@gmail.com>
2024-12-07 16:23:37 -0300

    using vscode theme again

---
+++ config/nvim/lua/custom/plugins/horizon.lua
@@ -0,0 +1,12 @@
+return {
+	--    "akinsho/horizon.nvim",
+	--    lazy = false,
+	--    config = function()
+	--        local cmd = vim.cmd
+	--        vim.opt.termguicolors = true
+	-- vim.g.high_contrast_theme = true
+	-- vim.cmd.colorscheme('horizon')
+	--        -- cmd("colorscheme horizon")
+	--        cmd("hi LineNr guifg=#5eacd3")
+	--    end
+}
